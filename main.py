40e3cfa14b57dbfdcfb7d256096c670f70636719
MIKE-HFFMNN <S2210710042@fhooe.at>
2022-12-31 15:30:34 +0100

    -m third session; accessing excel, but doesnt work yet 2

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import matplotlib as plt
+import matplotlib as mpl
 import numpy as np
-import pandas as pd
 import os
+import math
 from openpyxl import load_workbook
-from openpyxl.chart import Reference, BarChart
-import scipy.integrate as integrate
-import scipy.special as special
-import inquirer
 import tkinter as tk
 from tkinter import *
 
@@ -18,45 +15,51 @@ print(files)
 
 #Definition of basic variables
 g = 9.81
-t = 1
-
-#Lookup table of friction values
-ms = [0.65, 0.4, 0.2, 0.1, 0.1, 0, 0]
-md = [0.5, 0.35, 0.15, 0.08, 0.05, 0.35, 0.3]
-
-#Definition of basic formulas
-def normal(v):
-    return v*10**2
-def reaction(v):
-    return v/3.6*t
-def danger(v):
-    return normal(v)/2
-def distance(v):
-    return reaction(v)+normal(v)
-"""
-def Fn:
-    return rs*g*m
-"""
 
 #User input velocity
 root=Tk()
-def input():
-    v=txt_Box.get("1.0", "end-1c")
+root.geometry("600x200")
+v=0
+vm=0
+def inputv():
+    global v
+    v=int(txt_Box.get("1.0", "end-1c"))
     print("Velocity input: ", v, " kp/h")
-txt_Box=Text(root, height= 1, width=8)
+    global vm
+    vm=str(v / 3.6)
+    print("Velocity input: ", vm, " m/s")
+    return v
+    return vm
+txt_Box = Text(root, height=1, width=8)
 txt_Box.pack()
-attri_button=Button(root, height=1, width= 16, text="input velocity in kp/h", command=lambda: input())
+attri_button=Button(root, height=1, width= 16, text="input velocity in kp/h", command=lambda: inputv())
 attri_button.pack()
 mainloop()
 
+"""
 #User input vehicle mass
 root=Tk()
-def input():
-    m=txt_Box.get("1.0", "end-1c")
+root.geometry("600x200")
+m=0
+def inputm():
+    m=int(txt_Box.get("1.0", "end-1c"))
     print("Mass input: ", m, " kg")
 txt_Box=Text(root, height= 1, width=8)
 txt_Box.pack()
-attri_button=Button(root, height=1, width= 16, text="input mass in kg", command=lambda: input())
+attri_button=Button(root, height=1, width= 16, text="input mass in kg", command=lambda: inputm())
+attri_button.pack()
+mainloop()
+"""
+#User input inclination
+root=Tk()
+root.geometry("600x200")
+y=0
+def inputy():
+    y=int(txt_Box.get("1.0", "end-1c"))
+    print("Inclination input: ", y, "°")
+txt_Box=Text(root, height= 1, width=8)
+txt_Box.pack()
+attri_button=Button(root, height=1, width= 16, text="input inclination in °", command=lambda: inputy())
 attri_button.pack()
 mainloop()
 
@@ -67,7 +70,7 @@ svar = tk.StringVar()
 svar.set(servs[0])
 sr = servs[0]
 def _get(cur):
-    sr = cur
+    sr = str(cur)
     print("Road surface input: ", sr)
 drop = tk.OptionMenu(root, svar, command = _get, *servs)
 drop.grid(row=2, column=1)
@@ -78,7 +81,7 @@ root = tk.Tk()
 servs = ['dry', 'wet', 'aquaplaning']
 svar = tk.StringVar()
 svar.set(servs[0])
-sc = servs[0]
+sc = str(servs[0])
 def _get(cur):
     sc = cur
     print("Road condition input: ", sc)
@@ -91,55 +94,77 @@ wb = load_workbook('20_VCD1IL_CODING.xlsx', data_only=True)
 print(wb.sheetnames)
 ws = wb["Friction values"]
 
-if sr == 'concrete' and sc == 'dry':
-    rs = str(ws['C2'].value)
-    print("rs: ", rs)
-    rd = str(ws['D2'].value)
-    print("rd: ", rd)
-elif sr == 'concrete' and sc == 'wet':
-    rs = str(ws['C3'].value)
-    print("rs: ",rs)
-    rd = str(ws['D3'].value)
-    print("rd: ", rd)
-elif sr == 'ice' and sc == 'dry':
-    rs = str(ws['C4'].value)
-    print("rs: ",rs)
-    rd = str(ws['D4'].value)
-    print("rd: ", rd)
-elif sr == 'ice' and sc == 'wet':
-    rs = str(ws['C5'].value)
-    print("rs: ",rs)
-    rd = str(ws['D5'].value)
-    print("rd: ", rd)
-elif sr == 'water' and sc == 'aquaplaning':
-    rs = str(ws['C6'].value)
-    print("rs: ",rs)
-    rd = str(ws['D6'].value)
-    print("rd: ", rd)
-elif sr == 'gravel' and sc == 'dry':
-    rs = str(ws['C7'].value)
-    print("rs: ",rs)
-    rd = str(ws['D7'].value)
-    print("rd: ", rd)
-elif sr == 'sand' and sc == 'dry':
-    rs = str(ws['C8'].value)
-    print("rs: ",rs)
-    rd = str(ws['D8'].value)
-    print("rd: ", rd)
+ms = [cell.value for cell in ws['C'][1:]]
+md = [cell.value for cell in ws['D'][1:]]
+print(ms)
+print(md)
+
+if sr == "concrete" and sc == "dry":
+    rs = ms[0]
+    rd = md[0]
+elif sr == "concrete" and sc == "wet":
+    rs = [ms[1]]
+    rd = [md[1]]
+elif sr == "ice" and sc == "dry":
+    rs = [ms[2]]
+    rd = [md[2]]
+elif sr == "ice" and sc == "wet":
+    rs = [ms[3]]
+    rd = [md[3]]
+elif sr == "water" and sc == "aquaplaning":
+    rs = [ms[4]]
+    rd = [md[4]]
+elif sr == "gravel" and sc == "dry":
+    rs = [ms[5]]
+    rd = [md[5]]
+elif sr == "sand" and sc == "dry":
+    rs = [ms[6]]
+    rd = [md[6]]
 else:
     print("Failure - option not possible")
+print("rs: ", rs)
+print("rd: ", rd)
 
-"""
-Formulas for
-Fr = µ from excel*m*g
-Wk = (m*(v**2))/2
-Wf = integrate.quad(lambda x: special.jv(2.5,x), 0, 4.5)
+#Definition of basic formulas
+def calcsnormal(v):
+    snormal = v*10**2
+    return snormal
+def calcsreaction(vm):
+    sreaction = vm*t
+    return sreaction
+def calcsdanger(v):
+    sdanger = snormal(v)/2
+    return sdanger
+def calcdistance(v):
+    distance = sreaction(v)+snormal(v)
+    return distance
+def calcfn(g, m):
+    fn = m*g
+    return fn
+def calcamax(rs, g, y):
+    amax = rs*g*math.cos(math.radians(y))+g*math.sin(math.radians(y))
+    return amax
+def calcs(vm, a):
+    s = (vm^2)/(2*a)
+    return s
+def calctmax(rs, vm, y):
+    amax = rs * g * math.cos(math.radians(y)) + g * math.sin(math.radians(y))
+    return vm/amax
 
-print(f'normal: {normal(v)}')
-print(f'reaction: {reaction(v)}')
-print(f'danger: {danger(v)}')
-9print(f'distance: {distance(v)}')
+#call calculation of tmax
+tmax = calctmax(rs, vm, y)
+print(rs)
+print(vm, "m/s")
+print(tmax, "s")
+tr = abs(math.ceil(t))
+tv = np.arange(0, tr, 0.1)
+sv = [None] * len(tv)
+vv = [None] * len(tv)
+vv[0] = vm
+
+
 """
 #Doublecheck µ values
 print(rs)
 print(rd)
+"""
